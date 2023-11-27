eaa04da5b5f7a906567b02017c72885b26f413e0
naviddewman <navid.dewman@gmail.com>
2023-11-27 19:09:30 +1100

    build(deps): update checker.js and package-lock.json

--- checker.js
+++ checker.js
@@ -1,22 +1,40 @@
 const { Client, ActivityType } = require("discord.js");
-const OpenAi = require('openai');
+const OpenAI = require('openai');
+
+const openai = new OpenAI({apiKey: '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'});
+async function interpret(msgContent, msgTime) {
+    const response = await openai.chat.completions.create({
+      messages: [
+        { role: "system", content: "You only reply a time HH:MM based on when the user will arrive from the given message." },
+        { role: "user", content: 'message: ' + msgContent + ' ' + 'sent at: ' + msgTime },
+    ],
+      model: "gpt-3.5-turbo",
+    });
+  
+    return (response.choices[0]);
+}
 
 module.exports = {
     operating: false,
     instances: [],
 
-    init: function (msg) {
+    init: async function (msg) {
         // new instance object
         const inst = {
             msg: null,
             member: null,
-            value: null,
+            time: null,
         }
 
         this.operating = true;
         inst.msg = msg;
         this.member = msg.member;
         this.instances.push(inst);
+
+        const response = await interpret(msg.content, msg.createdAt);
+        console.log(response);
+
+        msg.reply('We will see about that.');
 
         // get time in message
 
@@ -33,3 +51,5 @@ module.exports = {
         }
     }
 };
+
+
