aa86884ae37d97b10d72af747c6c03ca0e90fbf7
naviddewman <navid.dewman@gmail.com>
2023-11-29 00:46:22 +1100

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Require the necessary discord.js classes
-const fs = require('fs');
-const path = require('path');
+const fs = require('node:fs');
+const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 require('dotenv').config();
 
